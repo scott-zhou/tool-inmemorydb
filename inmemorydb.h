a192683a4eee1c4025672709751b97ea7284cce1
scott-zhou <scott.cong.zhou@gmail.com>
2015-08-21 21:08:02 +0200

    Replace tab with 4 spaces.

--- inmemorydb.h
+++ inmemorydb.h
@@ -14,39 +14,39 @@
 #include <sys/sem.h>
 #include <errno.h>
 
-#define MAXNUMOFTABLE	100
-#define MAXNUMOFSEMS	256
+#define MAXNUMOFTABLE    100
+#define MAXNUMOFSEMS    256
 class CInmemoryDB{
 private:
-	//datamember
-	void *pShmData;
-	int semID;
-	int shmID;
-public:	
+    //datamember
+    void *pShmData;
+    int semID;
+    int shmID;
+public:
         CInmemoryDB(){pShmData = NULL;semID = -1;shmID = -1;};
         ~CInmemoryDB(){
-		if (pShmData != NULL) {
-			shmdt((char *)pShmData);
-			pShmData = NULL;
-		}
-	};
-	int create(const char *ipcPathName,int ipcid,int shmSize,int semNum, int operatorFlag = 0600);
-	int connect(const char *ipcPathName,int ipcid,int accessFlag = 0);
-	int createTable(int tableid,int tableSize);
-	void * getTablePData(int tableid);
-	int getDBSize(void);
-	int getTableSize(int tableid);
-	int lock(int tableid);
-	int unLock(int tableid);
-	int releaseInmemDB(void);
-	int isExist(void);
-	int getShmId(){
-		return	shmID;
-	};
+        if (pShmData != NULL) {
+            shmdt((char *)pShmData);
+            pShmData = NULL;
+        }
+    };
+    int create(const char *ipcPathName,int ipcid,int shmSize,int semNum, int operatorFlag = 0600);
+    int connect(const char *ipcPathName,int ipcid,int accessFlag = 0);
+    int createTable(int tableid,int tableSize);
+    void * getTablePData(int tableid);
+    int getDBSize(void);
+    int getTableSize(int tableid);
+    int lock(int tableid);
+    int unLock(int tableid);
+    int releaseInmemDB(void);
+    int isExist(void);
+    int getShmId(){
+        return    shmID;
+    };
 private:
-	int releaseShm();
-	int releaseSem();
+    int releaseShm();
+    int releaseSem();
 public:
-	int detatchShm(void);
+    int detatchShm(void);
 };
 #endif
